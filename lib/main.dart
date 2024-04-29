042a8a7c20bc408f1145ae8861b7fd6deae067d5
Toi Yoshimaru <0maru.dev@gmail.com>
2024-04-30 01:13:40 +0900

    GlobalKey を複数作成してページ毎に使い分ける

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'main.g.dart';
+
+final pageIndexProvider = StateProvider<int>((ref) => 0);
+
+@riverpod
+List<GlobalKey<NestedScrollViewState>> globalKeys(GlobalKeysRef ref) {
+  return [
+    GlobalKey<NestedScrollViewState>(debugLabel: '0'),
+    GlobalKey<NestedScrollViewState>(debugLabel: '1'),
+    GlobalKey<NestedScrollViewState>(debugLabel: '2'),
+    GlobalKey<NestedScrollViewState>(debugLabel: '3'),
+    GlobalKey<NestedScrollViewState>(debugLabel: '4'),
+    GlobalKey<NestedScrollViewState>(debugLabel: '5'),
+    GlobalKey<NestedScrollViewState>(debugLabel: '6'),
+  ];
+}
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
+
+final GlobalKey<NestedScrollViewState> globalKey = GlobalKey();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -69,8 +90,12 @@ class _FixedAppBar extends StatelessWidget {
       flexibleSpace: ColoredBox(
         color: Colors.red,
         child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-            child: Text('スクロールしても表示し続ける')),
+          padding: EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 12,
+          ),
+          child: Text('スクロールしても表示し続ける'),
+        ),
       ),
     );
   }
@@ -102,9 +127,8 @@ class _HideAppBar extends StatelessWidget {
   }
 }
 
-class _NestedScrollView extends StatelessWidget {
+class _NestedScrollView extends StatefulHookConsumerWidget {
   const _NestedScrollView({
-    super.key,
     required this.headers,
     required this.stackChildren,
   });
@@ -113,28 +137,37 @@ class _NestedScrollView extends StatelessWidget {
   final List<Widget> stackChildren;
 
   @override
+  ConsumerState<_NestedScrollView> createState() => _NestedScrollViewState();
+}
+
+class _NestedScrollViewState extends ConsumerState<_NestedScrollView> {
+  @override
   Widget build(BuildContext context) {
     return NestedScrollView(
+      key: ref.watch(globalKeysProvider)[ref.watch(pageIndexProvider)],
       physics: const BouncingScrollPhysics(
         parent: AlwaysScrollableScrollPhysics(),
       ),
       headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-        return headers;
+        return widget.headers;
       },
       body: Stack(
-        children: stackChildren,
+        children: widget.stackChildren,
       ),
     );
   }
 }
 
-class _PageView extends StatelessWidget {
-  const _PageView({super.key});
+class _PageView extends HookConsumerWidget {
+  const _PageView();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return PageView.builder(
-      itemCount: 10,
+      itemCount: ref.watch(globalKeysProvider).length,
+      onPageChanged: (index) {
+        ref.read(pageIndexProvider.notifier).update((state) => index);
+      },
       itemBuilder: (_, i) {
         return _InfinityScrollView(pageIndex: i);
       },
@@ -142,27 +175,52 @@ class _PageView extends StatelessWidget {
   }
 }
 
-class _InfinityScrollView extends StatelessWidget {
+class _InfinityScrollView extends StatefulHookConsumerWidget {
   const _InfinityScrollView({
-    super.key,
     required this.pageIndex,
   });
 
   final int pageIndex;
 
   @override
+  ConsumerState<_InfinityScrollView> createState() => _InfinityScrollViewState();
+}
+
+class _InfinityScrollViewState extends ConsumerState<_InfinityScrollView>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    // NestedScrollViewのスクロールを監視
+    // ただしPageViewでListViweを切り替えてもスクロール位置はNestedScrollViewのスクロール位置になる
+    // 個別にScrollControllerを管理したい場合には使えない
+    ref
+        .watch(globalKeysProvider)[ref.watch(pageIndexProvider)]
+        .currentState
+        ?.innerController
+        .addListener(() {
+      debugPrint(
+        'index: ${widget.pageIndex}, pixels: ${globalKey.currentState?.innerController.position.pixels}',
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
     return ListView.builder(
-      controller: ScrollController(),
       itemCount: 100,
       itemBuilder: (_, i) {
         return ColoredBox(
           color: i % 2 == 0 ? Colors.white : Colors.black12,
           child: ListTile(
-            title: Text('pageIndex: $pageIndex, index: $i'),
+            title: Text('pageIndex: ${widget.pageIndex}, index: $i'),
           ),
         );
       },
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
---
+++ lib/main.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$globalKeysHash() => r'ca668d4f74c2684aabdb3f166a63c22c6938fee9';
+
+/// See also [globalKeys].
+@ProviderFor(globalKeys)
+final globalKeysProvider =
+    AutoDisposeProvider<List<GlobalKey<NestedScrollViewState>>>.internal(
+  globalKeys,
+  name: r'globalKeysProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$globalKeysHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GlobalKeysRef
+    = AutoDisposeProviderRef<List<GlobalKey<NestedScrollViewState>>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
